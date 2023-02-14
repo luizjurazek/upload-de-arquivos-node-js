cde5370c38866e7b49ed2ca1c3be49c70433f705
Luiz Jurazek <luizjurazek@gmail.com>
2023-02-13 21:11:06 -0300

    Modelando BD

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(morgan('dev'));
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const mongoose = require('mongoose')
 
 
-app.use(require("./routes"));
 
-app.listen(3000);
+/**
+ * Database setup
+ */
+ mongoose.set('strictQuery', true); // Removendo warning 
+ mongoose.connect("mongodb://127.0.0.1/upload").then(() => {
+     console.log("Conectado ao mongo");
+ }).catch((err) => {
+     console.log("Erro ao se conectar: " + err);
+ });
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(morgan('dev'))
+
+app.use(require("./routes"))
+
+app.listen(3000)
